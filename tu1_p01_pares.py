1b714ff750928109ab932d9c426d67114d2107ee
Carlos Cares <carlos.cares@ceisufro.cl>
2023-08-20 18:07:05 -0400

    Add files via upload

---
+++ tu1_p01_pares.py
@@ -0,0 +1,5 @@
+#Problema 1 tutorial 1
+#Debe devolver los elementos en las posiciones pares de una lista de entrada
+#Por ahora la lista entrega la lista completa (elementos en UNA línea separados por blanco)
+lista = input().split()
+print(lista)
