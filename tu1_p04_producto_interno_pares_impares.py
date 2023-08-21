0b0bde4efe5469a656b1aca8085ea3c538204bc0
Carlos Cares <carlos.cares@ceisufro.cl>
2023-08-21 00:41:49 -0400

    Update tu1_p04_producto_interno_pares_impares.py
    Actualización problema 4

--- tu1_p04_producto_interno_pares_impares.py
+++ tu1_p04_producto_interno_pares_impares.py
@@ -1,17 +1,14 @@
 #Problema 4 tutorial 1
-#Para una secuencia con el siguiente significado 
-#Posición 0, un número que indica una posición p en la lista posterior
-#Posición 1, un elemento e
-#Posición 2 en adelante, una lista L separada por espacios
-#Proceso esperado: El programa debe insertar en la posición p de la lista L el elemento e
-#Si la posición p no existe debe agregarlo al final
+#Para una secuencia de números
+#Programe el producto interior (elemento a elemento)
+#de los números en las posiciones pares respecto de las impares
+#(los elementos que faltan deben ser 0) 
+#-------------------------------------------------------------
+#Ejemplo de Entrada
+# 1 2 3 6 2
+# debe consierarse (1 3 2) . (2 6 0) = 1x2 + 3x6 + 2x0 = 20
+#Ejemplo de salida 20
 #-------------------------------------
-#Ejemplo de entrada  2 q 10 20 30 40 50
-#Ejemplo de salida 10 20 q 30 40 50
-#-------------------------------------
-#Ejemplo de entrada 9 w a b c
-#Ejemplo de salida a b c w
-#-------------------------------------
-#Por ahora la lista entrega la lista completa (elementos en UNA línea separados por blanco)
-lista = input().split()
+#Por ahora la lista entrega los elementos pares agregado un cero
+lista = input().split()[0::2]+[0]
 print(lista)
