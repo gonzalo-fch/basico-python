56d814cae32c7b5b25fcd36b3cd09c2148b60390
Carlos Cares <carlos.cares@ceisufro.cl>
2023-08-20 23:59:23 -0400

    Update tu1_p02_agrega_en_pos.py

--- tu1_p02_agrega_en_pos.py
+++ tu1_p02_agrega_en_pos.py
@@ -7,10 +7,10 @@
 #Si la posición p no existe debe agregarlo al final
 #-----------------------------------------
 #Ejemplo de entrada  2 q 10 20 30 40 50
-#Ejemplo de salida [10, 20, q, 30, 40, 50]
+#Ejemplo de salida ['10', '20', 'q', '30', '40', '50']
 #------------------------------------------
 #Ejemplo de entrada 9 w a b c
-#Ejemplo de salida [a, b, c, w]
+#Ejemplo de salida ['a', 'b', 'c', 'w']
 #-------------------------------------
 #Por ahora la lista entrega la lista completa (elementos en UNA línea separados por blanco)
 lista = input().split()
